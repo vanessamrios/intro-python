3442b6295e1d2342b37ae5f33a7e1ba3f02c765d
Vanessa <vanessamrios@gmail.com>
2022-08-05 16:12:06 +0200

    feat: retorna uma lista de valores unicos na coluna industry

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
 
-    Must call `read`
+    jobs = read(path)
+    unique_industries = set()
+    for job in jobs:
+        if job["industry"]:
+            unique_industries.add(job["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
