0c2a219ed48242b1a92d9271c6e3a4d1e72bd70e
Vanessa <vanessamrios@gmail.com>
2022-08-06 13:33:11 +0200

    test: implementa teste unitério para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    word = "java"
+    report = count_ocurrences(path, word)
+    assert report == 676
+    assert type(report) is int
+    assert type(path) is str
+    assert type(word) is str
